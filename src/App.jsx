dd9c7d6640b2e14652ee6a99474f56f7dd192d7d
Luka Kolak <looka2449@rocketmail.com>
2025-04-14 12:58:29 +0200

    profile-visits

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import { useEffect, useState } from "react";
 
 import "./style/Reset.scss";
 
 const App = () => {
+  const [visits, setVisits] = useState(null);
+
+  useEffect(() => {
+    fetch("https://profile-visit-counter.onrender.com/api/visit")
+      .then((res) => res.json())
+      .then((data) => setVisits(data.visits));
+  }, []);
   return (
     <>
       <Nav />
-      <Hero />
+      <Hero visits={visits} />
       <About />
       <Skills />
       <Projects />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "../style/Hero.scss";
+import CountUp from "react-countup";
 
-const Hero = () => {
+const Hero = ({ visits }) => {
   return (
     <div className="hero">
       <h1>Luka Kolak</h1>
+      {visits !== null && (
+        <p className="visit-counter">
+          👁️ <CountUp end={visits} duration={4} /> posjeta profilu
+        </p>
+      )}
     </div>
   );
 };
